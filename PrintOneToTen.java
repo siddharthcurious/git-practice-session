ed95d02eec11ab8c2cbfc8b8a69ced61fddd195d
Siddharth Kumar <siddharth@Siddharths-MacBook-Air.local>
2022-10-31 11:09:19 +0530

    Added print number 1 to 100

--- PrintOneToTen.java
+++ PrintOneToTen.java
@@ -3,5 +3,11 @@ public class PrintOneToTen {
         for(int i = 1; i <= 10;i++){
             System.out.println(i);
         }
+
+        // print number 1 to 100
+
+        for(int i = 1; i <= 100;i++){
+            System.out.println(i);
+        }
     }
 }
